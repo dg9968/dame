14fb9146a2c3b6e6456fd6cee3abfe9c18abf1ab
dg9968 <daniel.galindo@me.com>
2016-06-28 17:04:39 -0400

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,10 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
 
-  config.action_mailer.mailgun_settings = {
+  config.action_mailer.smtp_settings = {
     :api_key              => ENV['api_key'],
     :domain               => ENV['domain']
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
 
-  config.action_mailer.mailgun_settings = {
+  config.action_mailer.smtp_settings = {
     :api_key              => ENV['api_key'],
     :domain               => ENV['domain']
   }
