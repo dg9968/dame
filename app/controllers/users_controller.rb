a9a4f142c5a33b74360d4062de75cc8fd9e2a859
dg9968 <daniel.galindo@me.com>
2016-06-29 07:54:02 -0400

    deliver_now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        UserMailer.sample_email(@user).deliver_later
+        UserMailer.sample_email(@user).deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
