4ba5783fef4dcbe21dac2e32b39864179778c13a
dg9968 <daniel.galindo@me.com>
2016-10-07 11:11:44 -0400

    Address module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_action :logged_in_user, only: [:index, :edit, :update, :show, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def new
   	@user = User.new
-    @user.build_address
+   
    end
 
   def create
@@ -34,9 +34,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+    
   def update
     @user = User.find(params[:id])
+     @user.build_address
    
     if @user.update_attributes(user_params)
       # Handle a successful update.
